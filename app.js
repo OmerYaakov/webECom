172d0850867069a537b29195f6abbd20b622a9e5
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-07-29 20:46:16 +0300

    fixed script to start and package.json to module type

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express';
+import routes from "./routes/routes.js";
+
 const app = express();
 
 // Set up EJS as view engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use('/', routes);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,29 @@
 import express from 'express';
 const router = express.Router();
 
-router.route('/').get()
+// Home page
+router.route('/').get((req, res) => {
+    res.render('index');
+});
 
-router.route('/:id')
+// Product page
+router.route('/product').get((req, res) => {
+    res.render('product');
+});
+
+// Admin page
+router.route('/admin').get((req, res) => {
+    res.render('admin');
+});
+
+// User settings page
+router.route('/user/settings').get((req, res) => {
+    res.render('user/settings');
+});
+
+// About us page
+router.route('/about').get((req, res) => {
+    res.render('about');
+});
+
 export default router;
